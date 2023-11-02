6a0a3de0ff999e91c1b8351362c9487436c177aa
Camilo Pereira <pereiracamilo51@gmail.com>
2023-11-02 18:44:00 -0500

    Agregando proyecto y modificando tamaño de las imagenes de proyectos

--- src/helpers/proyectos.js
+++ src/helpers/proyectos.js
@@ -1,4 +1,5 @@
 import Citas from "../img/proyectos/citas.png";
+import Cripto from "../img/proyectos/cripto.png";
 import EdmFestival from "../img/proyectos/edmFestival.png";
 import Gastos from "../img/proyectos/gastos.png";
 import GifExpert from "../img/proyectos/gif-expert.png";
@@ -13,6 +14,13 @@ export const proyectos = [
     tecnologias: ["React", "CSS"],
   },
   {
+    img: Cripto,
+    link: "https://cryptomonedas-camilo51.netlify.app/",
+    github: "https://github.com/camilo51/criptos-react",
+    titulo: "Cotiza Criptomonedas al Instante",
+    tecnologias: ["React", "Styled Components", "Api"],
+  },
+  {
     img: Citas,
     link: "https://citas-react-camilo51.netlify.app",
     github: "https://github.com/camilo51/citas_react_vite",
@@ -24,7 +32,7 @@ export const proyectos = [
     link: "https://lista-gasto-camilo51.netlify.app",
     github: "https://github.com/camilo51/gastos-react-firebase",
     titulo: "Lista De Gastos",
-    tecnologias: ["React", "CSS", "Styled Components"],
+    tecnologias: ["React", "CSS", "Styled Components", "Firebase"],
   },
   {
     img: EdmFestival,
